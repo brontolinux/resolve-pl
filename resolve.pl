5508bcc92338408ba4a43dadea4247694eadf45e
Marco Marongiu <brontolinux@gmail.com>
2020-10-27 18:51:25 +0100

    WIP: getting there, no working reverse yet

--- resolve.pl
+++ resolve.pl
@@ -6,6 +6,7 @@ use warnings ;
 use v5.10.1 ;
 
 #use Socket qw(inet_ntop inet_pton AF_INET AF_INET6);
+use Carp ;
 use Socket qw(:DEFAULT :addrinfo IPPROTO_TCP inet_ntop inet_pton);
 use Net::IP qw(ip_get_version);
 
@@ -15,7 +16,6 @@ $address_type_cleartext{AF_INET6()} = "ipv6" ;
 
 
 ENTRY:  foreach my $entry (@ARGV) {
-    my ( $name,   $aliases,  $addrtype,  $length,  @addrs ) ;
     my $entry_type = ip_get_version($entry) ;
 
     if (not defined $entry_type) {
@@ -27,16 +27,25 @@ ENTRY:  foreach my $entry (@ARGV) {
         }
 
         ADDRINFO:   foreach my $info (@addrinfo) {
+            my ($error, $address) = getnameinfo($info->{addr}, NI_NUMERICHOST, NIx_NOSERV) ;
+            
+            if ($error) {
+                report_error($entry,$error) ;
+                next ADDRINFO ;
+            }
+
             my %results ;
             $results{entry} = $entry ;
             $results{aliases} = $info->{canonname} if exists $info->{canonname} ;
-            $results{addrs} = [ $info->{addr} ] ;
+            $results{addrs} = [ $address ] ;
             $results{addrtype} = $info->{family} ;
             report_results(%results) ;
         }
+
     }
 
     else {
+        my ( $name,   $aliases,  $addrtype,  $length,  @addrs ) ;
         if ($entry_type == 4) {
             my $entry_address = inet_pton(AF_INET,$entry) ;
             ( $name,   $aliases,  $addrtype,  $length,  @addrs ) = gethostbyaddr($entry_address, AF_INET) ;
@@ -46,6 +55,13 @@ ENTRY:  foreach my $entry (@ARGV) {
             my $entry_address = inet_pton(AF_INET6,$entry) ;
             ( $name,   $aliases,  $addrtype,  $length,  @addrs ) = gethostbyaddr($entry_address, AF_INET6) ;
         }
+
+        my %results ;
+        $results{entry} = $entry ;
+        $results{aliases} = $aliases if $aliases ;
+        $results{addrs} = \@addrs ;
+        $results{addrtype} = $addrtype ;
+        report_results(%results) ;
     }
 
     
@@ -63,18 +79,13 @@ sub report_results {
         say "$entry aliases $aliases" if defined $aliases ;
     }
 
-    foreach my $packed_address (@addrs) {
-        my ($error, $address) = getnameinfo($packed_address, NI_NUMERICHOST, NIx_NOSERV) ;
-        if ($error) {
-            report_error($parms{entry},$error) ;
-            return ;
-        }
+    foreach my $address (@addrs) {
         say "$entry $address_type_cleartext{$addrtype} $address" ;
     }
 
 }
 
 sub report_error {
-    my ($entry, $error) ;
-    warn "While resolving $entry: error $error" ;
+    my ($entry, $error) = @_ ;
+    carp "While resolving $entry: error $error" ;
 }
